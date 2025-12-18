b9b534deb4fd0f3ddde3d24460989009bc55ac90
Sofia <sofia.rios@qaracter.com>
2025-12-18 12:42:58 +0000

    Creacion de menu falta implementacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        GlobalAccountRegistry gar = new GlobalAccountRegistry();
+
+
+    }
+
+    public boolean menu(Scanner sc){
+        boolean repetir = true;
+        int option = 0;
+
+        System.out.println("1. Add new account.");
+        System.out.println("2. Transfer money between accounts.");
+        System.out.println("3. Show accounts with more than some balance");
+        System.out.println("4. Close");
+
+        option = readInt(sc);
+
+        switch (option){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                repetir = false;
+                break;
+            default:
+                System.out.println("Not readed option");
+        }
+
+        return repetir;
+    }
+
+    public int readInt(Scanner sc){
+        int readed = sc.nextInt();
+        sc.nextLine();
+        return readed;
     }
 }

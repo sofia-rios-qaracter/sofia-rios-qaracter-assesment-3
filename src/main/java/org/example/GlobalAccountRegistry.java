ef3bbb8580362cd7aaa4eb20504ec68105f7e6bc
Sofia <sofia.rios@qaracter.com>
2025-12-18 12:36:24 +0000

    Implemetado Global Account Registry

--- src/main/java/org/example/GlobalAccountRegistry.java
+++ src/main/java/org/example/GlobalAccountRegistry.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GlobalAccountRegistry {
+    List<BankAccount> accounts;
+
+    public void GlobalAccoutRegistry(){
+        this.accounts = new ArrayList<>();
+    }
+
+    public boolean addAccount(BankAccount account){
+        return this.accounts.add(account);
+    }
+
+    public BankAccount findAccount(String number){
+        // Find any
+        return this.accounts.isEmpty() ? null : this.accounts.stream().filter(account -> account.getAccountNumber() == number).toList().get(0);
+    }
+
+    public double getTotalBalance(){
+        double balance = 0.0;
+
+        for(BankAccount account: accounts){
+            balance += account.getBalance();
+        }
+
+        return balance;
+    }
+
+    public List<BankAccount> getAccountsWithBalanceGreaterThan(double balance){
+        return this.accounts.stream().filter(account -> account.getBalance()>balance).toList();
+    }
 }

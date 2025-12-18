4e3f47e7ba049c8712dcebd8261a9a6c8c8d3737
Sofia <sofia.rios@qaracter.com>
2025-12-18 11:47:30 +0000

    Añadidos getters y setters
    Se han añadido los getters y setters pertinentes, futura implementacion
    esperable para mejorar setters

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -4,6 +4,7 @@ public class BankAccount {
     private String accountNumber;
     private String accountHolder;
     private double balance;
+    // private static int quantity = 1; Futura posible implementacion
 
     public BankAccount(String accountNumber, String accountHolder, double balance){
         this.accountNumber = accountNumber;
@@ -17,6 +18,37 @@ public class BankAccount {
         this(accountNumber, "Unknown");
     }
     public BankAccount(){
-        this("00000000-A");
+        this("00000000");
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    public void setAccountHolder(String accountHolder){
+        this.accountHolder = accountHolder;
+    }
+
+    public String getAccountHolder(){
+        return this.accountNumber;
+    }
+
+    public double withdraw(double withdraw){
+        this.balance -= Math.abs(withdraw);
+        return balance;
+    }
+
+    public double deposit(double deposit){
+        this.balance += Math.abs(deposit);
+
+        return balance;
+    }
+
+    public double getBalance(){
+        return this.balance;
     }
 }
